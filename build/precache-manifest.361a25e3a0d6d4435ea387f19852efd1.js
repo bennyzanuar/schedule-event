25551adecb1912742bedc607d2c2c04fc23d1788
Benny Zanuar <zanuar19@gmail.com>
2019-03-18 22:23:55 +0700

    wish me luck

---
+++ build/precache-manifest.361a25e3a0d6d4435ea387f19852efd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae1bc87d889acbe33238",
+    "url": "/static/js/main.0d988ac1.chunk.js"
+  },
+  {
+    "revision": "29221fbb26f4beb97900",
+    "url": "/static/js/2.6c98639f.chunk.js"
+  },
+  {
+    "revision": "ae1bc87d889acbe33238",
+    "url": "/static/css/main.b37dd878.chunk.css"
+  },
+  {
+    "revision": "29221fbb26f4beb97900",
+    "url": "/static/css/2.c5e0af90.chunk.css"
+  },
+  {
+    "revision": "3fe926ecd04b4e4c22f46237a5a31eae",
+    "url": "/index.html"
+  }
+];
