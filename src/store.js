13ff8df7cd20a9f4c8acb8aa412967cd13490965
Benny Zanuar <zanuar19@gmail.com>
2019-03-23 21:41:12 +0700

    update

--- src/store.js
+++ src/store.js
@@ -6,11 +6,9 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && 
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    )
-);
-
+const store = createStore(
+    rootReducer,
+    initialState, 
+    applyMiddleware(thunk)
+)
 export default store
